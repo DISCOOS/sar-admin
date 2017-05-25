9bd76bb0027488bfafd299aad2c48e76a9c2044d
stianmorsund <stian.rm@gmail.com>
2017-05-26 00:34:54 +0200

    Fixed datebug in sort-missions.pipe

--- src/app/missions/sort-missions.pipe.ts
+++ src/app/missions/sort-missions.pipe.ts
@@ -5,12 +5,13 @@ import { Mission } from '../models/models';
 @Pipe({ name: 'sortMissions' })
 export class SortMissionsPipe implements PipeTransform {
     transform(value: Mission[], args?: any[]) {
+        console.log("inside piep")
         if (!value || !value.sort) { return value; }
 
         // We sort the missions by date; newest first.
         return value.sort((a: Mission, b: Mission) => {
-            if (a.dateStart < b.dateStart) { return -1; }
-            if (a.dateStart > b.dateStart) { return 1; }
+            if (a.dateStart < b.dateStart) { return 1; }
+            if (a.dateStart > b.dateStart) { return -1; }
             return 0;
         });
     }
