616debbfa20bbea2213c053eedd90f71ad97bec0
Kenneth Gulbrandsøy <kenneth@discoos.org>
2017-08-19 10:44:08 +0200

    Updated logo

--- src/environments/environment.config.ts
+++ src/environments/environment.config.ts
@@ -26,5 +26,5 @@ export const config = {
     // SAR Status api endpoint
     // ---------------------------------------------
     //
-    SAR_STATUS_API_HOST: env.SAR_STATUS_API_HOST || 'https://sar-status-api.heroku.com'
+    SAR_STATUS_API_HOST: env.SAR_STATUS_API_HOST || 'https://sar-status-api.herokuapp.com'
 };
